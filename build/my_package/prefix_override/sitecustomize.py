f44db8de31c1f356861bb1396ccb47000a4d5cfc
Gregor Niehl <gregorniehl@web.de>
2025-04-05 10:32:19 +0200

    gitignore: Enforce ignoring build dirs

--- build/my_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/var/home/gregorni/Projects/hector-crawler/my_ros2_project/.pixi/envs/default':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/var/home/gregorni/Projects/hector-crawler/my_ros2_project/install/my_package'
