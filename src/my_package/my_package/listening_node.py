94480866d65fc4a8088bc99e856f1820adeda2d5
Gregor Niehl <gregorniehl@web.de>
2025-04-05 11:35:12 +0200

    led: Fix blinker code

--- src/my_package/my_package/listening_node.py
+++ src/my_package/my_package/listening_node.py
@@ -1,21 +1,23 @@
 import rclpy
+import RPi.GPIO as GPIO
 from rclpy.node import Node
 from std_msgs.msg import Empty
-
-from machine import Pin
 
 
 class MinimalSubscriber(Node):
     def __init__(self):
         super().__init__("minimal_subscriber")
-        self.led = Pin(25, Pin.OUT)
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(16, GPIO.OUT)
+
         queue_length = 5
         self.subscription = self.create_subscription(
             Empty, "led_blinker", self.listener_callback, queue_length
         )
 
     def listener_callback(self, msg):
-        self.led.Toggle()
+        GPIO.output(16, not GPIO.input(16))
         self.get_logger().info("Got signal")
 
 
@@ -31,6 +33,7 @@ def main(args=None):
     # when the garbage collector destroys the node object)
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
